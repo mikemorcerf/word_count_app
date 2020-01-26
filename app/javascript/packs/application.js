cec7dfaf88bb3c615ad5dc84507b49bd20cf9ffb
mikemorcerf <mikemorcerf@gmail.com>
2020-01-26 14:47:25 -0500

    yarn font awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 import '../stylesheets/application'
 import './bootstrap_custom.js'
-
+import "@fortawesome/fontawesome-free/js/all";
 
 require("@rails/ujs").start()
 require("turbolinks").start()
