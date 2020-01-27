96fad5974e03a5379d2f364fd0b86fcae2038b8e
mikemorcerf <mikemorcerf@gmail.com>
2020-01-26 19:23:59 -0500

    install Google charts

--- app/javascript/components/MyGraph.jsx
+++ app/javascript/components/MyGraph.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Chart from "react-google-charts"
 
 class MyGraph extends React.Component {
 	render () {
 		return (
-			<div>This is Graph react</div>
+			<div class="row d-flex justify-content-center text-center">
+				<Chart
+				  width={'800px'}
+				  height={'400px'}
+				  chartType="PieChart"
+				  loader={<div>Loading Chart</div>}
+				  data={[
+				    ['Pizza', 'Popularity'],
+				    ['Pepperoni', 33],
+				    ['Hawaiian', 26],
+				    ['Mushroom', 22],
+				    ['Sausage', 10],
+				    ['Anchovies', 9],
+				  ]}
+				  options={{
+				    title: 'Number of times words were mentioned',
+				    sliceVisibilityThreshold: 0,
+				  }}
+				  rootProps={{ 'data-testid': '7' }}
+				/>
+			</div>
 		)
 	}
 }
