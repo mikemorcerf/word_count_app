6de88a29b1eeb1b646f9fb52b0054433d3345242
mikemorcerf <mikemorcerf@gmail.com>
2020-01-26 21:16:49 -0500

    before adding table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,11 @@ class PagesController < ApplicationController
 
   def graph
   end
+
+  def retrieve_data
+  	retrieved_data = Hash.new
+  	Datapoint.all.each do |dp|
+  		retrieve_data[dp.word] += dp.word_count
+  	end
+  end
 end
--- app/javascript/components/MyGraph.jsx
+++ app/javascript/components/MyGraph.jsx
@@ -4,20 +4,14 @@ import Chart from "react-google-charts"
 class MyGraph extends React.Component {
 	render () {
 		return (
+
 			<div class="row d-flex justify-content-center text-center">
 				<Chart
 				  width={'800px'}
 				  height={'400px'}
 				  chartType="PieChart"
 				  loader={<div>Loading Chart</div>}
-				  data={[
-				    ['Pizza', 'Popularity'],
-				    ['Pepperoni', 33],
-				    ['Hawaiian', 26],
-				    ['Mushroom', 22],
-				    ['Sausage', 10],
-				    ['Anchovies', 9],
-				  ]}
+				  data={retrieved_data}
 				  options={{
 				    title: 'Number of times words were mentioned',
 				    sliceVisibilityThreshold: 0,
