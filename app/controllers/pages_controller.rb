e891fe5d76045051f0a1130cc335ee75e75f1c59
mikemorcerf <mikemorcerf@gmail.com>
2020-01-27 20:04:42 -0500

    add input paramenters for PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,14 @@ class PagesController < ApplicationController
   def graph
   end
 
-  def retrieve_data_for_graph
-	array_retrieved_data = Datapoint.where(:created_at=>5.days.ago..Time.now).group(:word).sum(:word_count).to_a
+  def retrieve_data_for_graph(from_time, to_time)
+	array_retrieved_data = Datapoint.where(:created_at=>from_time..to_time).group(:word).sum(:word_count).to_a
 	array_retrieved_data.unshift(["Word","Word Count"])
   end
   helper_method :retrieve_data_for_graph
 
-  def retrieve_data_for_table
-  	json_retrieved_data = Datapoint.where(:created_at=>5.days.ago..Time.now)
+  def retrieve_data_for_table(from_time, to_time)
+  	retrieved_data = Datapoint.where(:created_at=>from_time..to_time)
   end
   helper_method :retrieve_data_for_table
 end
